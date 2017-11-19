bdb7ff5409be1d4bedb37edd632ca1e2137bbb34
veredz <verose@live.com>
2017-11-19 22:55:07 +0200

    Fix legends and titles, calculate true error, save figures to file instead of showing on screen

--- EX1/Q1.py
+++ EX1/Q1.py
@@ -28,34 +28,46 @@ def part_a():
     plt.yticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1, 1.1])
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.legend()
-    plt.title('Sample points from probability')
+    plt.title('Sample points from distribution')
     fig = plt.gcf()
     fig.canvas.set_window_title('Programming Assignment: Question 1(a)')
 
-    plt.scatter(xs, ys)
+    plt.scatter(xs, ys, label='data points')
     for x_tick in [0.25, 0.5, 0.75]:
         plt.plot([x_tick, x_tick], [-0.1, 1.1], 'r')
-    for interval in intervals:
-        plt.plot(interval, [0.5, 0.5], 'b')
+    for i, interval in enumerate(intervals):
+        if i == 0:
+            plt.plot(interval, [0.5, 0.5], 'b', label='model')
+        else:
+            plt.plot(interval, [0.5, 0.5], 'b')
 
-    plt.show()  # todo: savefig
+    plt.legend()
+    plt.savefig('q1_part_a.png')
+    plt.clf()
 
 
 def calculate_true_error(intervals):
-    # todo: calculate true error
-    points = [0, 0.25, 0.5, 0.75, 1, intervals[0][0], intervals[0][1], intervals[1][0], intervals[1][1]]
-    points.sort()
+    # denote: A: [0, 0.25] U [0.5, 0.75]
+    # denote: B: [0.25, 0.5] U [0.75, 1]
+    len_a = 0
+    len_b = 0
 
-    last_point = -1
-    for point in points:
-        if last_point == -1:
-            last_point = point
-            continue
-        distance = point - last_point
+    for interval in intervals:
+        if 0 <= interval[0] <= 0.25:
+            len_a += max(0.0, min(0.25, interval[1]) - max(0, interval[0]))
+        if 0.25 < interval[0] < 0.75:
+            len_a += max(0.0, min(0.75, interval[1]) - max(0.5, interval[0]))
+            if 0.25 < interval[0] < 0.5:
+                len_b += max(0.0, min(0.5, interval[1]) - max(0.25, interval[0]))
+        if 0.25 < interval[1] < 0.5:
+            len_b += max(0.0, interval[1] - max(0.25, interval[0]))
+        if 0.75 < interval[1] < 1:
+            len_b += max(0.0, interval[1] - max(0.75, interval[0]))
+    # the error on A is 0.2 when predicting 1 and 0.8 when predicting 0 (on the remainder)
+    # the error on B is 0.9 when predicting 1 and 0.1 when predicting 0 (on the remainder)
+    total_error = len_a * 0.2 + len_b * 0.9 + (0.5 - len_a) * 0.8 + (0.5 - len_b) * 0.1
 
-
-    return 0.5
+    return total_error
 
 
 def part_c():
@@ -82,14 +94,15 @@ def part_c():
     # Plot the average empirical and true errors, averaged across the T runs, as a function of m
     plt.xlabel('m')
     plt.ylabel('error')
-    plt.legend()
-    plt.title('Question 1(a)')
+    plt.title('Empirical error vs True error')
     fig = plt.gcf()
     fig.canvas.set_window_title('Programming Assignment: Question 1(c)')
 
-    plt.scatter(m_range, average_empirical_error, marker='o', label='empirical')
-    plt.scatter(m_range, average_true_error, marker='+', label='true')
-    plt.show()  # todo: savefig
+    plt.scatter(m_range, average_empirical_error, marker='o', label='empirical error')
+    plt.scatter(m_range, average_true_error, marker='+', label='true error')
+    plt.legend()
+    plt.savefig('q1_part_c.png')
+    plt.clf()
 
 
 def part_d():
@@ -108,4 +121,4 @@ def part_e():
 
 
 if __name__ == "__main__":
-    part_a()
+    part_c()
